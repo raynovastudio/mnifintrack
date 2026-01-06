919b8870691cc7e14d93c57b4ef3980c38a1876e
raynovastudio <raynovastudio@gmail.com>
2026-01-06 23:07:26 +0100

    more 2

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
